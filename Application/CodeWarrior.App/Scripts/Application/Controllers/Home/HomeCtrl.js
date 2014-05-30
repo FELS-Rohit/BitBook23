527164f8d989523d763e3727b24a7b50cd28bb11
Atish Kumar Dipongkor <atish@bs-23.com>
2014-05-30 16:58:11 +0600

    feed page added

--- Application/CodeWarrior.App/Scripts/Application/Controllers/Home/HomeCtrl.js
+++ Application/CodeWarrior.App/Scripts/Application/Controllers/Home/HomeCtrl.js
@@ -2,12 +2,27 @@
 
 (function(app) {
     app.controller("HomeCtrl", [
-        "$scope", "identityService", function($scope, identityService) {
+        "$scope", "identityService", "apiService", function ($scope, identityService, apiService) {
             $scope.init = function() {
                 if (!identityService.isLoggedIn()) {
                     $scope.redirectToLogin();
+                } else {
+                    //apiService.get("/api/posts/").success(function (result) {
+                    //    if (result) {
+                    //        $scope.posts = result;
+                    //    }
+                    //});
                 }
             }();
+
+            $scope.addPost = function (post) {
+                $scope.addPostSubmitted = true;
+                if ($scope.AddPostForm.$valid) {
+                    apiService.post("api/posts", post).success(function (result) {
+                        console.log(result);
+                    });
+                }
+            };
         }
     ]);
 })(_$.app);
