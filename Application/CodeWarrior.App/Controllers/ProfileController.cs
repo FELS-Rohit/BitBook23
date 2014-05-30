0f81493af7ab4721a52b11a1daef4473c81808cb
Sheikh Abdur Rahman <skarahman@gmail.com>
2014-05-30 16:24:40 +0600

    profile api

---
+++ Application/CodeWarrior.App/Controllers/ProfileController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CodeWarrior.DAL.DbContext;
+using CodeWarrior.DAL.Interfaces;
+using CodeWarrior.Model;
+using MongoDB.AspNet.Identity;
+
+namespace CodeWarrior.App.Controllers
+{
+    public class ProfileController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+
+        public ProfileController(IApplicationDbContext applicationDbContext, IUserRepository userRepository)
+            : base(applicationDbContext)
+        {
+            _userRepository = userRepository;
+        }
+
+        // GET api/profile
+        public ApplicationUser Get(string id = null)
+        {
+            return null;
+        }
+    }
+}
