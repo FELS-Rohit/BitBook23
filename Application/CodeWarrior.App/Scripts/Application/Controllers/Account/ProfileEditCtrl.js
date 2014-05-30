aba3804d02d1a626335b3dac2d0208af7dd4931a
Shibbir Ahmed <shibbir.cse@gmail.com>
2014-05-30 13:13:44 +0600

    bug fixed when registering user

---
+++ Application/CodeWarrior.App/Scripts/Application/Controllers/Account/ProfileEditCtrl.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+
+(function(app) {
+    app.controller("ProfileEditCtrl", [
+        "$scope", "$location", "identityService", "notifierService", function ($scope, $location, identityService, notifierService) {
+            $scope.init = function() {
+                if (!identityService.isLoggedIn()) {
+                    $location.path("/");
+                } else {
+                    identityService.getUserInfo().success(function (data) {
+                        $scope.user = data;
+                        notifierService.notify({ responseType: "success", message: "Profile data fetched successfully." });
+                    });
+                }
+            }();
+        }
+    ]);
+})(_$.app);
--- Application/CodeWarrior.App/Scripts/Application/Controllers/BaseCtrl.js
+++ Application/CodeWarrior.App/Scripts/Application/Controllers/BaseCtrl.js
@@ -2,13 +2,30 @@
 
 (function(app) {
     app.controller("BaseCtrl", [
-        "$scope", "$location", "identityService", function($scope, $location, identityService) {
+        "$scope", "$location", "identityService", "apiService", function($scope, $location, identityService, apiService) {
             $scope.logout = function() {
                 identityService.logout().success(function() {
                     identityService.clearAccessToken();
                     identityService.clearAuthorizedUserData();
                     $location.path("/account/login");
                 });
+            };
+
+            $scope.search = function() {
+                if (identityService.isLoggedIn()) {
+                    var config = {
+                        params: {
+                            q: $scope.searchKey
+                        }
+                    };
+                    apiService.get("/api/users/", config).success(function(result) {
+                        $location.path("/users/searchResult");
+                    }).error(function(error) {
+                        console.log(error);
+                    });
+                } else {
+                    $location.path("/account/login");
+                }
             };
         }
     ]);
