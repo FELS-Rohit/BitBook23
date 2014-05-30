871bbba659a277595ff285bf16a266e8d95dfd86
Atish Kumar Dipongkor <atish@bs-23.com>
2014-05-30 09:21:06 +0600

    QuestionAddCtrl added

--- Application/CodeWarrior.App/Scripts/Application/Controllers/Question/QuestionAddCtrl.js
+++ Application/CodeWarrior.App/Scripts/Application/Controllers/Question/QuestionAddCtrl.js
@@ -1,27 +1,26 @@
 ﻿"use strict";
 
-(function (app) {
+(function(app) {
     app.controller("QuestionAddCtrl", [
-        "$scope", "signalRConnectionService", "identityService", function ($scope, signalRConnectionService, identityService) {
-            
+        "$scope", "signalRConnectionService", "identityService", function($scope, signalRConnectionService, identityService) {
+
             var userName = "";
             var signalRConnection;
             $scope.userNotification = 0;
             var accessToken = sessionStorage.getItem("accessToken");
             console.log(accessToken);
             signalRConnection = signalRConnectionService.getSignalRConnection(accessToken);
-            
 
-            $scope.addQuestion = function (question) {
+            $scope.addQuestion = function(question) {
                 signalRConnection.server.addQuestionNotification(question.title, userName);
             };
 
-            signalRConnection.client.sendUserNotification = function (message) {
-                    $scope.userNotification = ++$scope.userNotification;
-                    console.log(message);
-                    $scope.$apply();
-                };
-           
+            signalRConnection.client.sendUserNotification = function(message) {
+                $scope.userNotification = ++$scope.userNotification;
+                console.log(message);
+                $scope.$apply();
+            };
+
         }
     ]);
 })(_$.app);
